a1a220e0b2a5cf0e04416f6772a6b2598de2820f
Haidara7 <haidooora1@gmail.com>
2019-05-20 15:56:46 +0300

    js017

--- Exercises/Javascript-Basics-01/Step_5.js
+++ Exercises/Javascript-Basics-01/Step_5.js
@@ -1,3 +1,3 @@
 var number1 = prompt("enter the first number");
 var number2 = prompt("enter the second number");
-alert(number1*number2);
+alert("the result is" +number1*number2);
---
+++ Exercises/Javascript-Basics-01/Step_7.js
@@ -0,0 +1,3 @@
+var shoes = prompt("what is your shoe size?");
+var birth = prompt("what is your birhday year?");
+alert(((shoes*2) + 5) * 50 - birth + 1766);
