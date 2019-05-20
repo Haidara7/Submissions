ab855609640383d03430bb16d601bbd7140365e5
Haidara7 <haidooora1@gmail.com>
2019-05-20 15:44:17 +0300

    js014

---
+++ Exercises/Javascript-Basics-01/Step_4.js
@@ -0,0 +1,5 @@
+var name = prompt("what is your name ?");
+var surname = prompt("what is your surname ?");
+var city = prompt("what is your city ?");
+
+alert("Hello " + name + "Surname " + surname + "City " + city);
