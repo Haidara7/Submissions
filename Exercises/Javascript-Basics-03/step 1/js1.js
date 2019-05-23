9d1c5f56d604ef920c7624ecaefba43829d42c96
Haidara7 <haidooora1@gmail.com>
2019-05-23 17:16:34 +0300

    js31

---
+++ Exercises/Javascript-Basics-03/step 1/js1.js
@@ -0,0 +1,12 @@
+var x = document.querySelector("img");
+console.log(x);
+x.addEventListener("mouseover",w);
+function w (){
+    x.style.border = "3px solid red";
+}
+var b = document.querySelector("img");
+console.log(b);
+b.addEventListener("mouseout",kdsad);
+function kdsad(){
+    b.style.border ="none";
+}
