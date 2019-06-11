6c436b4b8fed551253ad6402bcb0010feecbaa01
Haidara7 <haidooora1@gmail.com>
2019-06-11 18:32:28 +0300

    step3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -4,5 +4,16 @@ const app = express ();
 app.get('/', (req, res) => {
     res.send('ok');
 });
+app.get('/test', (req, res)=> {
+    res.send({status:200, message:"ok"})
+}) 
+var today = new Date()
+var TIME = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+app.get('/time',(req,res)=>{
+    res.send({
+        status:200,
+         message:TIME})
+})
 
 app.listen(3000 ,() => console.log('listening on port 3000....'));
+
