78be05475463eb920c13b1b25e1271e1193b53b4
Haidara7 <haidooora1@gmail.com>
2019-06-12 11:34:07 +0300

    step4

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -14,6 +14,21 @@ app.get('/time',(req,res)=>{
         status:200,
          message:TIME})
 })
+app.get('/hello/:id?', (req, res) => {
+    if (req.params.id !== undefined){
+ res.send({status:200, message:"Hello," +req.params.id})
+    }else {
+    res.send({status:200, message:"Hello"})
+    
+}})
+app.get('/search',(req,res)=> {
+    if(req.query.s !== undefined && req.query.s !== "") {
+        res.send({status:200, message:"ok", data:req.query.s})
+    }
+    else {
+        res.send({status:500, error:true, message:"you have to provide a search"})
+    }
+})
 
 app.listen(3000 ,() => console.log('listening on port 3000....'));
 
