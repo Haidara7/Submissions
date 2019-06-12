fe2bea8839bfa397bd6c2ffb69106a40658cbea4
Haidara7 <haidooora1@gmail.com>
2019-06-12 11:55:16 +0300

    step5

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -30,5 +30,32 @@ app.get('/search',(req,res)=> {
     }
 })
 
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
+app.get('/movies/create',(req, res) => {
+    res.send({status:200, message:"ok"})
+
+})
+
+app.get('/movies/read',(req, res) => {
+
+        res.send({status:200, data:movies })
+
+})
+app.get('/movies/update',(req, res) => {
+    res.send({status:200, message:"udating"})
+
+})
+app.get('/movies/delete',(req, res) => {
+    res.send({status:200, message:"deleting"})
+
+})
+
+
 app.listen(3000 ,() => console.log('listening on port 3000....'));
 
